cd52cb36d29569a41902cde24b2bd1db8832d972
Lukas Vlcek <lukas.vlcek@gmail.com>
2014-05-29 16:43:47 +0200

    Preparation for new features.

--- script.js
+++ script.js
@@ -21,176 +21,171 @@
  * Data pump to pull data from Magnolia projectData REST end point
  * and push into appropriate DCP REST end point.
  *
- * @author Lukas Vlcek (lvlcek@redhat.com)
+ * @author lvlcek@redhat.com (Lukas Vlcek)
  */
 
-var messageCodes = {
-	INFO : "class='info'",
-	DEBUG : "class='debug'",
-	ERROR : "class='error'"
-}
+$(document).ready(function(){
 
-var logMessage = function(message, opt_code) {
-	var code = opt_code || messageCodes.INFO;
-	$("#log").append("<li "+code+">"+message+"</li>");
-}
+	var messageCodes = {
+		INFO : "class='info'",
+		DEBUG : "class='debug'",
+		ERROR : "class='error'"
+	};
 
-/**
- * @private
- */
-var xsableInputElements_ = function(boolean) {
-	$.each(["#source_url", "#target_url", "#startButton"], function(i, element){
-		$(element).attr('disabled', boolean)
-	});
-}
-
-/**
- * Disable input elements.
- */
-var disableInputElements = function() {
-	xsableInputElements_(true);
-}
-
-/**
- * Enable input elements.
- */
-var enableInputElements = function() {
-	xsableInputElements_(false);
-}
-
-/**
- * Reset progress bar and set new max value.  Defaults to 100.
- * @param {number=} opt_max
- */
-var resetProgressBar = function(opt_max) {
-	var max = opt_max || 100;
-	var progressBar = $("#progressBar");
-	progressBar.attr('max',max);
-	progressBar.attr('value',0);
-}
-
-/**
- * Update progress bar to given value.
- * @param {number}
- */
-var setProgressBar = function(value) {
-	var progressBar = $("#progressBar");
-	progressBar.attr('value',value);
-}
-
-/**
- * @return {number}
- */
-var getProgressBarValue = function() {
-	var value = $("#progressBar").attr('value');
-	return +value; // force number type
-}
-
-/**
- * Indeterminate progress bar.
- */
-var indeterminateProgressBar = function() {
-	var progressBar = $("#progressBar");
-	progressBar.attr('max',null);
-	progressBar.attr('value',null);
-}
-
-var processData = function(data) {
-
-	if (data && data.hits && data.hits.hits) {
-		var hits = data.hits.hits;
-		if (hits.length && hits.length > 0) {
-			
-			var target_url = $("#target_url").val();
-			var totalHits = hits.length;
-			logMessage("Sending out " + totalHits + " requests...");
-			resetProgressBar(totalHits);
-
-			// prepare XHR config
-			var ajaxconf = {
-				type: 'POST',
-				//url: na,
-				crossDomain: true,
-				// data: na,
-				dataType: 'JSON',
-				processData: false,
-				contentType: 'application/json'
-				// ,async:false
-			};
-
-			// setup credentials if provided
-			var username = $.trim($("#username").val());
-			var password = $.trim($("#password").val());
-			if (username.length > 0 && password.length > 0) {
-				ajaxconf["username"] = username;
-				ajaxconf["password"] = password;
-				ajaxconf["xhrFields"] = { withCredentials: true	};
-			}
-
-			// create deferred chain
-			var dfr = $.Deferred();
-
-			$.each(hits, function(i, hit){
-
-				var data = hit._source;
-				ajaxconf["data"] = JSON.stringify(data);
-				ajaxconf["url"] = target_url+"/content/"+data.sys_content_type+"/"+data.sys_content_id;
-
-				dfr.then(
-					$.ajax(
-						ajaxconf
-					).done(function(){
-						logMessage("[OK] hit ["+(i+1)+"] for ["+data.sys_content_id+"]");
-					}).fail(function(){
-						logMessage("[ERROR] hit ["+(i+1)+"] for ["+data.sys_content_id+"]", messageCodes.ERROR);
-					}).always(function(){
-						setProgressBar(getProgressBarValue()+1);
-					})
-				);
-			});
-		}
-		dfr.then(function(){
-			logMessage("Done.");
-			resetProgressBar();
-			enableInputElements();
-		});
-		dfr.resolve();
-	} else {
-		logMessage("Malformed data!");
-		resetProgressBar();
-		enableInputElements();
-	}
-}
-
-/**
- * Click handler that is bind to a start button.
- */
-var startButtonClicked = function() {
-	
-	$("#log").empty();
-	indeterminateProgressBar();
-
-	var source_url = $("#source_url").val();
-	logMessage("Getting source data from " +source_url);
-
-	disableInputElements();
-
-	$.ajax({
-	  dataType: "json",
-	  url: source_url
-	}).fail(function(err){
-		logMessage("Got error" + err);
-	  	resetProgressBar();
-	}).done(function(data){
-		logMessage("Got source data.");
-		processData(data);
-	});
-}
-
-var start = function(jQuery) {
+	var log = $("#log");
 	var startButton = $("#startButton");
 	startButton.bind("click", startButtonClicked);
-}
 
-// Standard jQuery pattern that kickstarts on page ready.
-$(document).ready(start);
+	/**
+	 * Click handler that is bind to a start button.
+	 */
+	function startButtonClicked() {
+		log.empty();
+		indeterminateProgressBar();
+
+		var source_url = $("#source_url").val();
+		logMessage("Getting source data from " +source_url);
+
+		disableInputElements();
+
+		$.ajax({
+			dataType: "json",
+			url: source_url
+		}).fail(function(err){
+			logMessage("Got error: " + err);
+			resetProgressBar();
+		}).done(function(data){
+			logMessage("Got source data.");
+			processData(data);
+		});
+	}
+
+	function processData(data) {
+		if (data && data.hits && data.hits.hits) {
+
+			var hits = data.hits.hits;
+			var dfr = $.Deferred();
+
+			if (hits.length && hits.length > 0) {
+
+				var target_url = $("#target_url").val();
+				var totalHits = hits.length;
+				logMessage("Sending out " + totalHits + " requests...");
+				resetProgressBar(totalHits);
+
+				// prepare common XHR config
+				var ajaxconf = {
+					type: 'POST',
+					crossDomain: true,
+					dataType: 'JSON',
+					processData: false,
+					contentType: 'application/json'
+					// ,async:false
+				};
+
+				// setup credentials if provided
+				var username = $.trim($("#username").val());
+				var password = $.trim($("#password").val());
+				if (username.length > 0 && password.length > 0) {
+//					ajaxconf.headers = { Authorization: 'Basic ' + btoa(username + ':' + password) };
+					ajaxconf.xhrFields = { withCredentials: true };
+				}
+
+				$.each(hits, function(i, hit){
+
+					var conf = {};
+					$.extend(true, conf, ajaxconf);
+
+					var data = hit._source;
+					conf["data"] = JSON.stringify(data);
+					conf["url"] = target_url+"/content/"+data.sys_content_type+"/"+data.sys_content_id;
+
+					dfr.then(
+						function() {
+							return $.ajax(conf)
+								.done( function() { logMessage("[OK] hit ["+(i+1)+"] for ["+data.sys_content_id+"]"); } )
+								.fail( function() { logMessage("[ERROR] hit ["+(i+1)+"] for ["+data.sys_content_id+"]", messageCodes.ERROR); } )
+								.always( function() { setProgressBar(getProgressBarValue()+1); } );
+						}
+					);
+				});
+			}
+			dfr.then(function(){
+				logMessage("Done.");
+				resetProgressBar();
+				enableInputElements();
+			});
+			dfr.resolve();
+		} else {
+			logMessage("Malformed data!");
+			resetProgressBar();
+			enableInputElements();
+		}
+	}
+
+	/**
+	 * Indeterminate progress bar.
+	 */
+	function indeterminateProgressBar() {
+		var progressBar = $("#progressBar");
+		progressBar.attr('max',null);
+		progressBar.attr('value',null);
+	}
+
+	/**
+	 * @return {number}
+	 */
+	function getProgressBarValue() {
+		var value = $("#progressBar").attr('value');
+		return +value; // force number type
+	}
+
+	/**
+	 * Update progress bar to given value.
+	 * @param {number} value
+	 */
+	function setProgressBar(value) {
+		var progressBar = $("#progressBar");
+		progressBar.attr('value',value);
+	}
+
+	/**
+	 * Disable input elements.
+	 */
+	function disableInputElements() {
+		xsableInputElements_(true);
+	}
+
+	/**
+	 * Enable input elements.
+	 */
+	function enableInputElements() {
+		xsableInputElements_(false);
+	}
+
+	/**
+	 * Reset progress bar and set new max value.  Defaults to 100.
+	 * @param {number=} opt_max
+	 */
+	function resetProgressBar(opt_max) {
+		var max = opt_max || 100;
+		var progressBar = $("#progressBar");
+		progressBar.attr('max',max);
+		progressBar.attr('value',0);
+	}
+
+	function logMessage(message, opt_code) {
+		var code = opt_code || messageCodes.INFO;
+		log.append("<li "+code+">"+message+"</li>");
+	}
+
+	/**
+	 * @private
+	 */
+	function xsableInputElements_(boolean) {
+		$.each(["#source_url", "#target_url", "#startButton"], function(i, element){
+			$(element).attr('disabled', boolean)
+		});
+	}
+
+});
